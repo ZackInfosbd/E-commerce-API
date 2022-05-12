93006803edd1d11a554f85d97ec654336a4332df
ZackInfosbd <68170594+ZackInfosbd@users.noreply.github.com>
2022-05-12 07:54:59 +0200

    logout route

--- controllers/authController.js
+++ controllers/authController.js
@@ -52,7 +52,11 @@ const login = async (req, res) => {
 };
 
 const logout = async (req, res) => {
-  res.send('logout route');
+  res.cookie('token', 'logout', {
+    httpOnly: true,
+    expires: new Date(Date.now()),
+  });
+  res.status(StatusCodes.OK).json({ msg: 'you are logged out' });
 };
 
 module.exports = {
