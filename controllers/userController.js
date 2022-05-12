f51f84af6f27d23b5ef197ec5e2fbf81054b775d
ZackInfosbd <68170594+ZackInfosbd@users.noreply.github.com>
2022-05-12 12:08:44 +0200

    update user password controller

--- controllers/userController.js
+++ controllers/userController.js
@@ -26,12 +26,26 @@ const showCurrentUser = async (req, res) => {
   res.status(StatusCodes.OK).json({ user: req.user });
 };
 
-const updateUser = async (req, res) => {
-  res.send(req.body);
-};
+const updateUser = async (req, res) => {};
 
 const updateUserPassword = async (req, res) => {
-  res.send(req.body);
+  const { oldPassword, newPassword } = req.body;
+
+  if (!oldPassword || !newPassword) {
+    throw new CustomErrors.BadRequestError('Please provide both values');
+  }
+
+  const user = await User.findOne({ _id: req.user.userId });
+
+  const isPasswordCorrect = await user.comparePassword(oldPassword);
+
+  if (!isPasswordCorrect) {
+    throw new CustomErrors.UnauthenticatedError('Invalid Credentials');
+  }
+  user.password = newPassword;
+  await user.save();
+
+  res.status(StatusCodes.OK).json({ msg: 'Success! password updated' });
 };
 
 module.exports = {
