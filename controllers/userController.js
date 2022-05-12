8c1c856a0bd3cf60b49a88cb37da9472fd8f062a
ZackInfosbd <68170594+ZackInfosbd@users.noreply.github.com>
2022-05-12 11:29:08 +0200

    show current user controller

--- controllers/userController.js
+++ controllers/userController.js
@@ -21,12 +21,15 @@ const getSingleUser = async (req, res) => {
 
   res.status(StatusCodes.OK).json({ user });
 };
+
 const showCurrentUser = async (req, res) => {
-  res.send('show Current user');
+  res.status(StatusCodes.OK).json({ user: req.user });
 };
+
 const updateUser = async (req, res) => {
   res.send(req.body);
 };
+
 const updateUserPassword = async (req, res) => {
   res.send(req.body);
 };
