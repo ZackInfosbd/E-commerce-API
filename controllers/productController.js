31aea453d2de3e50cc355fb360c76ad9b53a48b3
ZackInfosbd <68170594+ZackInfosbd@users.noreply.github.com>
2022-05-13 10:25:59 +0200

    Product CRUD operations controllers

--- controllers/productController.js
+++ controllers/productController.js
@@ -7,20 +7,66 @@ const createProduct = async (req, res) => {
 
   const product = await Product.create(req.body);
 
-  res.status(StatusCodes.CREATED).json({ product });
+  res
+    .status(StatusCodes.CREATED)
+    .json({ msg: 'Success! Product has been created', product });
 };
+
 const getAllProducts = async (req, res) => {
-  res.send(' get All Products route');
+  const products = await Product.find({});
+  res.status(StatusCodes.OK).json({ nbHits: products.length, products });
 };
+
 const getSingleProduct = async (req, res) => {
-  res.send('get Single Product route');
+  const { id: productId } = req.params;
+  const product = await Product.findOne({ _id: productId });
+
+  if (!product) {
+    throw new CustomError.NotFoundError(`
+    No product with id ${req.params.id}
+    `);
+  }
+
+  res.status(StatusCodes.OK).json({ product });
 };
+
 const updateProduct = async (req, res) => {
-  res.send('update Product route');
+  const { id: productId } = req.params;
+
+  const product = await Product.findOneAndUpdate({ _id: productId }, req.body, {
+    new: true,
+    runValidators: true,
+  });
+
+  if (!product) {
+    throw new CustomError.NotFoundError(`
+    No product with id : ${productId}
+    `);
+  }
+
+  res
+    .status(StatusCodes.OK)
+    .json({ msg: 'Success! Product has been updated', product });
 };
+
 const deleteProduct = async (req, res) => {
-  res.send('delete Product route');
+  const { id: productId } = req.params;
+
+  const product = await Product.findOne({ _id: productId });
+
+  if (!product) {
+    throw new CustomError.NotFoundError(`
+    No product with id : ${productId}
+    `);
+  }
+
+  await product.remove();
+
+  res
+    .status(StatusCodes.OK)
+    .json({ msg: 'Success! Product has been deleted', product });
 };
+
 const uploadImage = async (req, res) => {
   res.send('upload Image route');
 };
