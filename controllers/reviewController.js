4cbf9da176aefb4aa02cc43acee6f20040069d5f
ZackInfosbd <68170594+ZackInfosbd@users.noreply.github.com>
2022-05-13 12:44:06 +0200

    Create Review Controller

--- controllers/reviewController.js
+++ controllers/reviewController.js
@@ -1,10 +1,39 @@
 const User = require('../Models/User');
 const Product = require('../Models/Product');
+const Review = require('../Models/Review');
 const { StatusCodes } = require('http-status-codes');
 const CustomError = require('../errors');
+const { checkPermissions } = require('../utils');
 
 const createReview = async (req, res) => {
-  res.send(' create Review route');
+  const { product: productId, rating, title, comment } = req.body;
+
+  const isValidproduct = await Product.findOne({ _id: productId });
+
+  if (!rating || !title || !comment) {
+    throw new CustomError.BadRequestError('Please provide all the values');
+  }
+
+  if (!isValidproduct) {
+    throw new CustomError.NotFoundError(`No product with id : ${productId}`);
+  }
+
+  const userAlreadyReviewed = await Review.findOne({
+    product: productId,
+    user: req.user.userId,
+  });
+
+  if (userAlreadyReviewed) {
+    throw new CustomError.BadRequestError(
+      'You have already reviewed the product'
+    );
+  }
+
+  req.body.user = req.user.userId;
+
+  const review = await Review.create(req.body);
+
+  res.status(StatusCodes.CREATED).json({ review });
 };
 
 const getAllReviews = async (req, res) => {
