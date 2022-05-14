bc3f2b3e5222b35fcb573e6e32eb4416a250e2f7
ZackInfosbd <68170594+ZackInfosbd@users.noreply.github.com>
2022-05-14 10:36:45 +0200

    Review update controller

--- controllers/reviewController.js
+++ controllers/reviewController.js
@@ -57,7 +57,32 @@ const getSingleReview = async (req, res) => {
 };
 
 const updateReview = async (req, res) => {
-  res.send(' update Review route');
+  const { id: reviewId } = req.params;
+  const { rating, title, comment } = req.body;
+
+  const review = await Review.findOne({ _id: reviewId });
+
+  if (!review) {
+    throw new CustomError.NotFoundError(`
+    No review with id : ${reviewId}
+    `);
+  }
+
+  if ((!rating, title, comment)) {
+    throw new CustomError.BadRequestError(`
+Please provide all values
+    `);
+  }
+
+  checkPermissions(req.user, review.user);
+
+  review.rating = rating;
+  review.title = title;
+  review.comment = comment;
+
+  await review.save();
+
+  res.status(StatusCodes.OK).json({ msg: 'review has been updated' });
 };
 
 const deleteReview = async (req, res) => {
