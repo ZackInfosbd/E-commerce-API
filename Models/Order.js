a2df296806310e556df7168488de7ec9d0cc3f2f
ZackInfosbd <68170594+ZackInfosbd@users.noreply.github.com>
2022-05-14 15:13:51 +0200

    Order + singleCartItem schema model initialization

---
+++ Models/Order.js
@@ -0,0 +1,69 @@
+const mongoose = require('mongoose');
+
+const SingleCartItemSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  image: {
+    type: String,
+    required: true,
+  },
+  price: {
+    type: Number,
+    required: true,
+  },
+  amount: {
+    type: String,
+    required: true,
+  },
+  product: {
+    type: mongoose.Types.ObjectId,
+    ref: 'Product',
+    required: true,
+  },
+});
+
+const OrderSchema = new mongoose.Schema(
+  {
+    tax: {
+      type: Number,
+      required: [true, 'Please provide Tax Fees'],
+    },
+    shippingFee: {
+      type: Number,
+      required: [true, 'Please provide Shiiping Fees'],
+    },
+    subTotal: {
+      type: Number,
+      required: true,
+    },
+    total: {
+      type: Number,
+      required: true,
+    },
+    cartItems: [SingleCartItemSchema],
+    status: {
+      type: String,
+      enum: ['pending', 'failed', 'paid', 'delivered', 'canceled'],
+      default: 'pending',
+    },
+    user: {
+      type: mongoose.Types.ObjectId,
+      ref: 'User',
+      required: true,
+    },
+    clientSecret: {
+      type: String,
+      required: true,
+    },
+    paymentIntentId: {
+      type: String,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+module.exports = mongoose.model('Order', OrderSchema);
